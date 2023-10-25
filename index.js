df97221c50c0d89192299a0f54d4b2b01fe40da7
sagar-wandile <wandilesagar2926@gmail.com>
2023-10-25 23:59:06 +0530

    add get post

--- index.js
+++ index.js
@@ -22,48 +22,74 @@ const productSchema = new Schema({
 });
 
 const Product = model('Product', productSchema);
-const products = [];
- 
 
-app.get('/product', async (req, res) => {
-    const {name,description, price, productImage,brand} =req.body ;
+app.get('/product/:_id', async (req, res) => {
+ const {_id} = req.params ;
 
-    const prod = new Product({
-         name:name,
-        description:description,
-         price:price,
-         productImage:productImage,
-         brand:brand
-     })
-     const savedProduct = await prod.save();
+    const products = await Product.findOne({_id:_id})
      res.json({
         success: true,
-        data: savedProduct,
+        data:products,
         message:'Successfully got Products'
     })
 });
 
 app.post('/product', (req, res) => {
-    const {name,description, price, productImage,brand} =req.body ;
-  if (!name){
-    
+    const {name,description,price, productImage,brand} =req.body ;
+ 
+    if (!name){
+    return res.json({
+        success: false,
+        message:'Plese enter product name',
+   });
   }
-  return res.json({
-   success: false,
-   message:'Plese enter product name',
-});
+  if (!description){
+    return res.json({
+        success: false,
+        message:'Plese enter product description',
+   });
+  }
+  if (!price){
+    return res.json({
+        success: false,
+        message:'Plese enter product price',
+   });
+  }
+  if (!productImage){
+    return res.json({
+        success: false,
+        message:'Plese enter product product Url',
+   });
+  }
+  if (!brand){
+    return res.json({
+        success: false,
+        message:'Plese enter product brand',
+   });
+  }
+  const newProduct = new Product({
+         name:name,
+         description:description,
+         price:price,
+         productImage:productImage,
+         brand:brand
+  });
+  const savedProduct = newProduct.save();
+  req.json({
+    success:true,
+    data: savedProduct,
+    message: 'Product saved successfully',
+  })
 
 });
 
 app.get('/products',async (req, res) => {
 
-    const {}=req.query;
-  
-    const product = await Product.find();
+    const products = await Product.find();
   
       res.json({
           success: true,
-          data: product,
+          data: products,
           message:'Successfully fetch all product',
         })
   });
